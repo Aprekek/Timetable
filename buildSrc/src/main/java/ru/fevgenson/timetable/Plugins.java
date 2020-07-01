adf5e1f4536835ee2006dbce12a596097371ba37
fevgenson <fevgenson@gmail.com>
2020-07-01 15:04:32 +0700

    TT-10 Потеряный код стайл

--- buildSrc/src/main/java/ru/fevgenson/timetable/Plugins.java
+++ buildSrc/src/main/java/ru/fevgenson/timetable/Plugins.java
@@ -1,10 +1,10 @@
 package ru.fevgenson.timetable;
 
 public class Plugins {
-	public static String ANDROID_APPLICATION = "com.android.application";
-	public static String ANDROID_LIBRARY = "com.android.library";
-	public static String KOTLIN_ANDROID = "kotlin-android";
-	public static String KOTLIN_EXTENSIONS = "kotlin-android-extensions";
-	public static String KOTLIN_KAPT = "kotlin-kapt";
-	public static String NAVIGATION_SAVE_ARGS_KOTLIN = "androidx.navigation.safeargs.kotlin";
+    public static String ANDROID_APPLICATION = "com.android.application";
+    public static String ANDROID_LIBRARY = "com.android.library";
+    public static String KOTLIN_ANDROID = "kotlin-android";
+    public static String KOTLIN_EXTENSIONS = "kotlin-android-extensions";
+    public static String KOTLIN_KAPT = "kotlin-kapt";
+    public static String NAVIGATION_SAVE_ARGS_KOTLIN = "androidx.navigation.safeargs.kotlin";
 }
