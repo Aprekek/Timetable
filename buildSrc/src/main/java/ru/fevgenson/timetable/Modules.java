d6f3e81dc6320d3a1fa3f1d142cf4cff16e54559
fevgenson <fevgenson@gmail.com>
2020-07-07 14:52:14 +0700

    TT-17 Создал модуль dictionary

--- buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
+++ buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
@@ -4,6 +4,7 @@ public class Modules {
     public static final String MAIN = ":features:main";
     public static final String TIMETABLE = ":features:timetable";
     public static final String HOMEWORK = ":features:homework";
+    public static final String DICTIONARY = ":features:dictionary";
     public static final String NAVIGATION = ":libraries:navigation";
     public static final String THEMES = ":libraries:themes";
 }
