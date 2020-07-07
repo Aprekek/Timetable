e8a40f7596c1d3f03f77d149a637955f754084b0
fevgenson <fevgenson@gmail.com>
2020-07-07 14:45:54 +0700

    TT-17 Создал модуль timetable

--- buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
+++ buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
@@ -3,6 +3,7 @@ package ru.fevgenson.timetable;
 public class Modules {
     public static final String APP = ":app";
     public static final String MAIN = ":features:main";
+    public static final String TIMETABLE = ":features:timetable";
     public static final String NAVIGATION = ":libraries:navigation";
     public static final String THEMES = ":libraries:themes";
 }
