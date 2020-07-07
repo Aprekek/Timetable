8a0f97626297a18d4ed9ae1959fbd82975d1b971
fevgenson <fevgenson@gmail.com>
2020-07-07 15:13:06 +0700

    TT-17 Создал фрагменты, поправил цвета статусбаров с теме

---
+++ features/dictionary/src/main/java/ru/fevgenson/timetable/features/dictionary/presentation/DictionaryFragment.kt
@@ -0,0 +1,6 @@
+package ru.fevgenson.timetable.features.dictionary.presentation
+
+import androidx.fragment.app.Fragment
+import ru.fevgenson.timetable.features.dictionary.R
+
+class DictionaryFragment : Fragment(R.layout.fragment_dictionary)
