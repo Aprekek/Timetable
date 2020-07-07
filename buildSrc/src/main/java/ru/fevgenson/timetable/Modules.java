642526beb46ca917029e27b20c95c5d394542d68
fevgenson <fevgenson@gmail.com>
2020-07-07 14:49:01 +0700

    TT-17 Создал модуль homework

--- buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
+++ buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
@@ -1,9 +1,9 @@
 package ru.fevgenson.timetable;
 
 public class Modules {
-    public static final String APP = ":app";
     public static final String MAIN = ":features:main";
     public static final String TIMETABLE = ":features:timetable";
+    public static final String HOMEWORK = ":features:homework";
     public static final String NAVIGATION = ":libraries:navigation";
     public static final String THEMES = ":libraries:themes";
 }
