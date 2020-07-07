a36bd5a746a85c028be03637ecd86558b858a43b
Aprekek <mhlshamburov@gmail.com>
2020-07-07 18:04:59 +0700

    #1 Создал модуль lessoncreate

--- buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
+++ buildSrc/src/main/java/ru/fevgenson/timetable/Modules.java
@@ -2,6 +2,7 @@ package ru.fevgenson.timetable;
 
 public class Modules {
     public static final String APP = ":app";
+    public static final String LESSON_CREATE = ":features:lessoncreate";
     public static final String MAIN = ":features:main";
     public static final String NAVIGATION = ":libraries:navigation";
     public static final String THEMES = ":libraries:themes";
