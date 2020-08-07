a3d5683fd5574de1fce3b0330e028384b94d11eb
aprekek <mhlshamburov@gmail.com>
2020-08-07 18:51:44 +0700

    #1 Переместил EditTextBindingAdapter в Core

--- libraries/core/src/main/java/ru/fevgenson/timetable/libraries/core/presentation/bindingadapters/EditTextBindingAdapter.kt
+++ libraries/core/src/main/java/ru/fevgenson/timetable/libraries/core/presentation/bindingadapters/EditTextBindingAdapter.kt
@@ -1,12 +1,12 @@
-package ru.fevgenson.timetable.features.lessoncreate.presentation.bindingadapters
+package ru.fevgenson.timetable.libraries.core.presentation.bindingadapters
 
 import android.view.MotionEvent
 import android.view.View
 import androidx.databinding.BindingAdapter
 import com.google.android.material.textfield.TextInputEditText
 
-@BindingAdapter("startingPhoneSlots")
-fun TextInputEditText.setStartingPhoneSlots(slots: String) {
+@BindingAdapter("startingSlots")
+fun TextInputEditText.setupStartingSlots(slots: String) {
     setOnTouchListener { view: View, motionEvent: MotionEvent ->
         view.performClick()
         if (text?.isEmpty()!!) {
