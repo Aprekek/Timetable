66656134eb7f01626e64a6a4409bff1829d65d8a
fevgenson <fevgenson@gmail.com>
2020-08-16 17:17:06 +0700

    #5 Верстка предмета без учета времени

---
+++ features/timetable/src/main/java/ru/fevgenson/timetable/features/timetable/presentation/bindingadapters/LessonBindingAdapters.kt
@@ -0,0 +1,56 @@
+package ru.fevgenson.timetable.features.timetable.presentation.bindingadapters
+
+import android.view.View
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import ru.fevgenson.timetable.features.timetable.R
+
+@BindingAdapter("type", "teacher")
+fun TextView.setTypeAndTeacher(type: String?, teacher: String?) {
+    when {
+        type != null && teacher != null -> {
+            text = context.getString(
+                R.string.timetable_mask_with_brackets,
+                type,
+                teacher
+            )
+            visibility = View.VISIBLE
+        }
+        type != null -> {
+            text = type
+            visibility = View.VISIBLE
+        }
+        teacher != null -> {
+            text = teacher
+            visibility = View.VISIBLE
+        }
+        else -> {
+            visibility = View.GONE
+        }
+    }
+}
+
+@BindingAdapter("housing", "classroom")
+fun TextView.setHousingAndClassroom(housing: String?, classroom: String?) {
+    when {
+        housing != null && classroom != null -> {
+            text = context.getString(
+                R.string.timetable_mask_with_comma,
+                housing,
+                classroom
+            )
+            visibility = View.VISIBLE
+        }
+        housing != null -> {
+            text = classroom
+            visibility = View.VISIBLE
+        }
+        classroom != null -> {
+            text = classroom
+            visibility = View.VISIBLE
+        }
+        else -> {
+            visibility = View.GONE
+        }
+    }
+}
