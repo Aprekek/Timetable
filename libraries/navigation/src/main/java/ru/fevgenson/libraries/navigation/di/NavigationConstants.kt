225532f3695cfa6bedf8e317e9a15e0be806de51
fevgenson <fevgenson@gmail.com>
2020-08-24 12:18:15 +0700

    #5 Убрал опечатку, поправил кодстайл

--- libraries/navigation/src/main/java/ru/fevgenson/libraries/navigation/di/NavigationConstants.kt
+++ libraries/navigation/src/main/java/ru/fevgenson/libraries/navigation/di/NavigationConstants.kt
@@ -3,6 +3,7 @@ package ru.fevgenson.libraries.navigation.di
 object NavigationConstants {
 
     object LessonCreate {
+
         const val WEEK_TYPE = "WEEK_TYPE"
         const val DAY = "DAY"
         const val OPEN_TYPE = "OPEN_TYPE"
