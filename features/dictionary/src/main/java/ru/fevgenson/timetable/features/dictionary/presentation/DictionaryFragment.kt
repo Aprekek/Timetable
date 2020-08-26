dc05abcc3d9e78b87713f08b47af4387a407a7dd
aprekek <mhlshamburov@gmail.com>
2020-08-26 17:13:27 +0700

    #20 Настроил корректное отображение chip в TabLayout

--- features/dictionary/src/main/java/ru/fevgenson/timetable/features/dictionary/presentation/DictionaryFragment.kt
+++ features/dictionary/src/main/java/ru/fevgenson/timetable/features/dictionary/presentation/DictionaryFragment.kt
@@ -1,11 +1,13 @@
 package ru.fevgenson.timetable.features.dictionary.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.chip.Chip
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import ru.fevgenson.timetable.features.dictionary.R
@@ -24,8 +26,9 @@ class DictionaryFragment : Fragment() {
     ): View? {
         initBinding(inflater, container)
         initViewPager2()
-        initCategoryTabs()
-
+        initLayoutManager()
+        initTabLayoutMediator()
+        Log.d("B", "3")
         return binding.root
     }
 
@@ -41,7 +44,35 @@ class DictionaryFragment : Fragment() {
             CategoriesViewPagerAdapter(resources.getStringArray(R.array.categories))
     }
 
-    private fun initCategoryTabs() {
+    private fun initLayoutManager() {
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.initColors(R.color.blue, R.color.white)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                tab?.initColors(R.color.light_grey, R.color.black)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+
+    private fun TabLayout.Tab.initColors(
+        backgroundColor: Int,
+        textColor: Int
+    ) {
+        if (customView is Chip) {
+            (customView as Chip).apply {
+                setTextColor(resources.getColor(textColor))
+                chipBackgroundColor = resources.getColorStateList(backgroundColor)
+            }
+        }
+    }
+
+    private fun initTabLayoutMediator() {
         TabLayoutMediator(
             binding.tabLayout,
             binding.viewPager
@@ -53,7 +84,13 @@ class DictionaryFragment : Fragment() {
                 false
             )
             tabCategoryBinding.categoryChip.text = resources.getStringArray(R.array.categories)[i]
-            tab.customView = tabCategoryBinding.root
+            tabCategoryBinding.categoryChip.setOnClickListener {
+                tab.parent?.selectTab(tab) ?: throw NullPointerException(
+                    "DictionaryFragment::initCategoryTabs() - TabLayout must be not null"
+                )
+            }
+            tab.customView = tabCategoryBinding.categoryChip
+            tab.view.isClickable = false
         }.attach()
     }
 }
---
+++ features/dictionary/src/main/java/ru/fevgenson/timetable/features/dictionary/presentation/bindingadapters/TabLayoutWithChipTabBindingAdapter.kt
@@ -0,0 +1,46 @@
+package ru.fevgenson.timetable.features.dictionary.presentation.bindingadapters
+
+import android.content.res.ColorStateList
+import androidx.databinding.BindingAdapter
+import com.google.android.material.chip.Chip
+import com.google.android.material.tabs.TabLayout
+
+@BindingAdapter(
+    "backgroundColorSelected",
+    "backgroundColorUnselected",
+    "textColorSelected",
+    "textColorUnselected"
+)
+fun TabLayout.setColorStateList(
+    backgroundColorSelected: ColorStateList,
+    backgroundColorUnselected: ColorStateList,
+    textColorSelected: ColorStateList,
+    textColorUnselected: ColorStateList
+) {
+    addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            tab?.setColors(backgroundColorSelected, textColorSelected)
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {
+            tab?.setColors(backgroundColorUnselected, textColorUnselected)
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {
+        }
+
+    })
+}
+
+fun TabLayout.Tab.setColors(
+    backgroundColor: ColorStateList,
+    textColor: ColorStateList
+) {
+    if (customView is Chip) {
+        (customView as Chip).apply {
+            setTextColor(textColor)
+            chipBackgroundColor = backgroundColor
+        }
+    }
+}
