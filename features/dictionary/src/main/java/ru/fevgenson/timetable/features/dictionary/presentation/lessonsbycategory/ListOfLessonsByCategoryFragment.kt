1827eea82e6f895e87a9992bf4ffaa26f206b584
aprekek <mhlshamburov@gmail.com>
2020-09-08 19:13:00 +0700

    #20-Словарь правка

--- features/dictionary/src/main/java/ru/fevgenson/timetable/features/dictionary/presentation/lessonsbycategory/ListOfLessonsByCategoryFragment.kt
+++ features/dictionary/src/main/java/ru/fevgenson/timetable/features/dictionary/presentation/lessonsbycategory/ListOfLessonsByCategoryFragment.kt
@@ -61,8 +61,6 @@ class ListOfLessonsByCategoryFragment : Fragment() {
     }
 
     private fun initObserver() {
-        viewModel.lessons.observe(viewLifecycleOwner, {
-            adapter.submitList(it)
-        })
+        viewModel.lessons.observe(viewLifecycleOwner) { adapter.submitList(it) }
     }
 }
